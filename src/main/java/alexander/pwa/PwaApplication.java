10e9404b908b32955076880a663f2fa42459efe7
MELVIN ALEXANDER PEREZ PEREZ <19792127@clases.edu.sv>
2026-06-08 09:01:34 -0600

    Add files via upload

---
+++ src/main/java/alexander/pwa/PwaApplication.java
@@ -0,0 +1,13 @@
+package alexander.pwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwaApplication.class, args);
+	}
+
+}
